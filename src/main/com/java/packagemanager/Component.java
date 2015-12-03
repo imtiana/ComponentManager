bcc9cadd19086c3f3e964ebd455a552eacacdfd8
imtiana <imjihyung@gmail.com>
2015-12-02 23:22:20 -0800

    add Components class

--- src/main/com/java/packagemanager/Component.java
+++ src/main/com/java/packagemanager/Component.java
@@ -1,7 +1,41 @@
 package com.java.packagemanager;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Tiana Im on 2015-12-02.
  */
 public class Component {
+    private String name;
+    private boolean isInstalled;
+    private Set<Component> dependencySet;
+    private Set<Component> supportSet;
+
+    public Component(String name) {
+        this.name = name;
+        isInstalled = false;
+        dependencySet = new HashSet<>();
+        supportSet = new HashSet<>();
+    }
+
+    public void changeName(String newName) {
+        name = newName;
+        return;
+    }
+
+    public void Install() {
+        if (!isInstalled) {
+            isInstalled = true;
+        }
+    }
+
+    public void Remove() {
+        // TO DO
+    }
+
+    public String toString() {
+        return name;
+    }
+
 }
